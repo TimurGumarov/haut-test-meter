11bd7199bd89ae963ce44c85cc7eb5c1d03bcd6b
TimurGumarov <29134829+TimurGumarov@users.noreply.github.com>
2023-12-15 21:59:01 +0100

    chore: vite config quick fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'haut-test-meter/',
+    base: '/haut-test-meter/',
     plugins: [svelte()],
 })
