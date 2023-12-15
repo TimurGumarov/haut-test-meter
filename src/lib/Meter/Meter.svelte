ecfb6e1069bc5bf87d02c4127862a4ae20f504c0
TimurGumarov <29134829+TimurGumarov@users.noreply.github.com>
2023-12-15 12:23:55 +0100

    feat: added base of meter component

---
+++ src/lib/Meter/Meter.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import type { SegmentSize } from './segment.types'
+    import Icon from './Icon.svelte'
+    import Segment from './Segment.svelte'
+
+    export let min: number = 0
+    export let max: number = 1
+    export let optimum: number = 0.65
+    export let value: number = 0
+
+    let segmentSize: SegmentSize = 'big'
+</script>
+
+<div class="meter">
+    <Icon name={'Moon'} />
+    <div class="bar">
+        <Segment size={segmentSize} />
+        <Segment size={segmentSize} />
+        <Segment size={segmentSize} />
+        <Segment size={segmentSize} />
+        <Segment size={segmentSize} />
+        <Segment size={segmentSize} />
+        <Segment size={segmentSize} />
+    </div>
+    <Icon name={'Sun'} />
+</div>
+
+<style>
+    .meter {
+        display: flex;
+        flex-flow: row;
+        align-items: center;
+        justify-content: center;
+        gap: 28px;
+    }
+    .bar {
+        display: flex;
+        flex-flow: row;
+        align-items: center;
+        gap: 10px;
+    }
+</style>
