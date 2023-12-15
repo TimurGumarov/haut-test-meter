fa502146d7e941dbf64271666d3fbbd97d11df63
TimurGumarov <29134829+TimurGumarov@users.noreply.github.com>
2023-12-15 23:32:44 +0100

    chore: histoire settings changes

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,4 +3,9 @@ import { HstSvelte } from '@histoire/plugin-svelte'
 
 export default defineConfig({
     plugins: [HstSvelte()],
+    routerMode: 'hash',
+    outDir: 'dist/histoire/',
+    vite: {
+        base: '/haut-test-meter/histoire/',
+    },
 })
