bc61a4b8fdf3d373edef2219421ad82c01ce2e0f
TimurGumarov <29134829+TimurGumarov@users.noreply.github.com>
2024-01-18 19:09:37 +0100

    refactor: added min max inputs; deleted errors; scaled functionality

--- src/lib/Meter/Meter.svelte
+++ src/lib/Meter/Meter.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import { onMount } from 'svelte'
-    import type { SegmentSize } from './segment.types'
-
     import Icon from './Icon.svelte'
     import Segment from './Segment.svelte'
 
@@ -10,14 +7,12 @@
     export let optimum: number = 0.65
     export let value: number = 0
 
-    let error = ''
+    let animationRange: number
+    $: animationRange = (max - min) / 5
+
     // Value difference between divisions
-    let segmentTick: number = max / 6
-
-    let breakpoint: number = 320
-    let segmentSize: SegmentSize = 'big'
-
-    let meter: HTMLDivElement
+    let segmentTick: number
+    $: segmentTick = (max - min) / 6
 
     let iconMoonState: boolean = true
     let iconSunState: boolean = false
@@ -29,110 +24,49 @@
         !iconMoonState && (iconMoonState = !iconMoonState)
         iconSunState && (iconSunState = !iconSunState)
     }
-
-    function checkBreakpointAndSetSegmentSize() {
-        const conteiner = meter.parentElement
-        const conteinerWidth = meter.parentElement?.offsetWidth
-        if (conteiner && conteinerWidth && conteinerWidth > breakpoint) {
-            segmentSize != 'big' && (segmentSize = 'big')
-        } else {
-            segmentSize != 'small' && (segmentSize = 'small')
-        }
-    }
-
-    onMount(() => {
-        if (min < 0 || max < 0 || optimum < 0)
-            error =
-                "'min', 'max' and 'optimum' values must be greater than zero"
-        if (!error && min >= max)
-            error = "'min' value must be lower than 'max' value"
-        if (!error && (optimum > max || optimum < min))
-            error =
-                "'optimum' value must be within the range of 'min' and 'max' values"
-
-        checkBreakpointAndSetSegmentSize()
-
-        // This event handler needs for Presentation component only
-        // Can be removed for production
-        meter.parentElement?.addEventListener(
-            'resize',
-            checkBreakpointAndSetSegmentSize
-        )
-        return () => {
-            meter.parentElement?.removeEventListener(
-                'resize',
-                checkBreakpointAndSetSegmentSize
-            )
-        }
-    })
 </script>
 
-<div class="meter" bind:this={meter} data-testid="meter-container">
-    {#if !error}
-        <Icon name={'Moon'} active={iconMoonState} />
-        <div class="bar">
-            <Segment
-                size={segmentSize}
-                {min}
-                max={segmentTick}
-                mid={min}
-                {value}
-            />
-            <Segment
-                size={segmentSize}
-                {min}
-                max={segmentTick * 2}
-                mid={segmentTick}
-                {value}
-            />
-            <Segment
-                size={segmentSize}
-                min={segmentTick}
-                max={segmentTick * 3}
-                mid={segmentTick * 2}
-                {value}
-            />
-            <Segment
-                size={segmentSize}
-                min={segmentTick * 2}
-                max={segmentTick * 4}
-                mid={segmentTick * 3}
-                {value}
-            />
-            <Segment
-                size={segmentSize}
-                min={segmentTick * 3}
-                max={segmentTick * 5}
-                mid={segmentTick * 4}
-                {value}
-            />
-            <Segment
-                size={segmentSize}
-                min={segmentTick * 4}
-                max={segmentTick * 6}
-                mid={segmentTick * 5}
-                {value}
-            />
-            <Segment
-                size={segmentSize}
-                min={segmentTick * 5}
-                {max}
-                mid={max}
-                {value}
-            />
-        </div>
-        <Icon name={'Sun'} active={iconSunState} />
-    {:else}
-        <p class="error">Error: {error}</p>
-    {/if}
+<div class="meter" data-testid="meter-container">
+    <Icon name={'Moon'} active={iconMoonState} />
+    <div class="bar">
+        <Segment segmentValue={min} bind:globalValue={value} {animationRange} />
+        <Segment
+            segmentValue={min + segmentTick}
+            bind:globalValue={value}
+            {animationRange}
+        />
+        <Segment
+            segmentValue={min + segmentTick * 2}
+            bind:globalValue={value}
+            {animationRange}
+        />
+        <Segment
+            segmentValue={min + segmentTick * 3}
+            bind:globalValue={value}
+            {animationRange}
+        />
+        <Segment
+            segmentValue={min + segmentTick * 4}
+            bind:globalValue={value}
+            {animationRange}
+        />
+        <Segment
+            segmentValue={min + segmentTick * 5}
+            bind:globalValue={value}
+            {animationRange}
+        />
+        <Segment
+            segmentValue={min + segmentTick * 6}
+            bind:globalValue={value}
+            {animationRange}
+        />
+    </div>
+    <Icon name={'Sun'} active={iconSunState} />
 </div>
 
 <style>
-    .error {
-        font-family: sans-serif;
-        color: red;
-    }
     .meter {
+        container: meter / inline-size;
         display: flex;
         flex-flow: row;
         align-items: center;
@@ -141,8 +75,8 @@
         padding: 8px;
     }
     .bar {
-        display: flex;
-        flex-flow: row;
+        display: grid;
+        grid-template-columns: repeat(7, 10px);
         align-items: center;
         min-height: 42px;
         gap: 10px;
@@ -150,6 +84,7 @@
     @container (width < 320px) {
         .bar {
             min-height: 36px;
+            grid-template-columns: repeat(7, 6px);
         }
     }
 </style>
